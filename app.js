2c66a90e6b935007bc36137c08fcd5c89f37f129
eocampo52 <eocampo52@gmail.com>
2024-08-01 08:18:13 -0500

    added winner and loser classes logic

--- app.js
+++ app.js
@@ -1,31 +1,52 @@
 const p1Button = document.getElementById("p1Button");
 const p2Button = document.getElementById("p2Button");
+const resetButton = document.getElementById("reset");
+const winningScoreSelect = document.getElementById("playto");
 const p1Display = document.getElementById("p1Display");
 const p2Display = document.getElementById("p2Display");
 
-let isGameOver = false;
 let p1Score = 0;
-let p2Score = 0
-let winningScore = 5;
+let p2Score = 0;
+let winningScore = 3;
+let isGameOver = false;
 
 p1Button.addEventListener("click", function () {
-  if(!isGameOver) {
+  if (!isGameOver) {
     p1Score += 1;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
+    }
+    p1Display.textContent = p1Score;
   }
-  if(p1Score === winningScore) {
-    isGameOver = true
-  }
-  p1Display.textContent = p1Score;
 });
 
-p1Button.addEventListener("click", function () {
-  if(!isGameOver) {
-    p1Score += 1;
+p2Button.addEventListener("click", function () {
+  if (!isGameOver) {
+    p2Score += 1;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
+    }
+    p2Display.textContent = p2Score;
   }
-  if(p1Score === winningScore) {
-    isGameOver = true
-  }
-  p1Display.textContent = p1Score;
 });
 
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
 
+resetButton.addEventListener("click", reset);
+
+function reset() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("winner", "loser");
+}
